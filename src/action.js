6d481da1010c37e40a540a8af6be6c8de29da70f
woodRock <j.r.h.wood98@gmail.com>
2021-02-06 23:20:21 +1300

    Comment thank you on a new Pull Request

--- src/action.js
+++ src/action.js
@@ -1,5 +1,18 @@
+const core = require("@actions/core");
+const github = reuqire("@actions/github");
+
 async function run() {
-  console.log("Hello, world!");
+  const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+  const otokit = github.getOcktokit(GITHUB_TOKEN);
+
+  const { context = {} } = github;
+  const { pull_request } = context.payload;
+
+  await otokit.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: `Thank you for submitting a pull request! We will try to review this as soon as we can.`,
+  });
 }
 
 run();
