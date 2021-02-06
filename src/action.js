b64c004dd525e24c236325a8c3d1b14391c80a82
woodRock <j.r.h.wood98@gmail.com>
2021-02-06 23:28:56 +1300

    finished; should work!

--- src/action.js
+++ src/action.js
@@ -8,10 +8,17 @@ async function run() {
   const { context = {} } = github;
   const { pull_request } = context.payload;
 
+  const TENOR_TOKEN = core.getInput("TENOR_TOKEN");
+  const randomPos = Math.round(Math.random() * 1000);
+  const url = `https://api.tenor.com/v1/search?q=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
+  const response = await fetch(url);
+  const { results } = await response.json();
+  const gifUrl = results[0].media[0].tinygif.url;
+
   await otokit.issues.createComment({
     ...context.repo,
     issue_number: pull_request.number,
-    body: `Thank you for submitting a pull request! We will try to review this as soon as we can.`,
+    body: `Thank you for submitting a pull request! We will try to review this as soon as we can.\n\n<img src="${gifUrl}" alt="thank you" />`,
   });
 }
 
