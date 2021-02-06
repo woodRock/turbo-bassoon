6342934ba3f776bfc3907db3c0e15a9dbfbcd204
woodRock <j.r.h.wood98@gmail.com>
2021-02-06 23:21:51 +1300

    Fix import statement

--- src/action.js
+++ src/action.js
@@ -1,5 +1,5 @@
 const core = require("@actions/core");
-const github = reuqire("@actions/github");
+const github = require("@actions/github");
 
 async function run() {
   const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
